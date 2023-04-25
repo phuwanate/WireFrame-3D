d7148f13d1cb7725c12bbef8dbd4869599b1940d
Firstphu <phuwanate.lpk@gmail.com>
2023-04-25 13:20:57 +0700

    250423

--- test.c
+++ test.c
@@ -3,26 +3,45 @@
 
 int main()
 {
-	int bpp;
-	int size_line;
-	int endian;
-	int count_w = 0;
-	int count_h = 0;
+
+	int x1 = 0;
+	int x2 = 800;
+	int y1 = 0;
+	int y2 = 600;
+	int x = x1;
+	int y = y1;
+	int dx = x2 - x1;
+	int dy = y2 - y1;
+	int p = (2*dy) - (2*dx);
 	void *mlx_ptr = mlx_init();
 	void *win_ptr = mlx_new_window(mlx_ptr, 800, 600, "mlx 42");
 
-	int	i = 0;
-	while (i < 600)
+	while (x <= x2)
 	{
-		int j = 0;
-		while (j < 800)
-		{
-			if (i == j)
-				mlx_pixel_put(mlx_ptr, win_ptr,j,i, 0x00FF00);
-			j++;
+		mlx_pixel_put(mlx_ptr, win_ptr,x,y, 0x00FF00);
+		x++;
+		if(p < 0)
+			p = p + (2 * dy);
+		else
+		{	
+			p = p + (2 * dy) - (2 * dx);
+			y++;
 		}
-		i++;
 	}
-	mlx_loop(mlx_ptr);
+	y = 0;
+	while (x >= 0)
+	{
+		mlx_pixel_put(mlx_ptr, win_ptr,x,y, 0xFF0000);
+		x--;
+		if(p < 0)
+			p = p + (2 * dy);
+		else
+		{	
+			p = p + (2 * dy) - (2 * dx);
+			y++;
+		}
+	}
+
 	
+	mlx_loop(mlx_ptr);
 }
